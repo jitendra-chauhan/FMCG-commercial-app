11c867d8a7b2fd32aec7cd8101e198dc86cb10bf
jitendra chauhan <jitendra.chauhan@artoon.in>
2022-09-22 18:25:11 +0530

    add static aleart message

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -4,6 +4,11 @@ export = Object.freeze({
   PASSWORD_NOT_MATCH: 'password no match',
   SUPER_ADMIN_YOUR_ACCOUNT_NOT_ACTIVE: 'no acctive',
   USER_ROLE_DETAILS_NOT_FOUND: 'role not found!',
-  ACCOUNT_ALREADY_EXISTS : "account already exists!",
-  YOU_ARE_NOT_AUTHORIZE : "you are not authorize!" 
+  ACCOUNT_ALREADY_EXISTS: 'account already exists!',
+  YOU_ARE_NOT_AUTHORIZE: 'you are not authorize!',
+  CATEGORY_ALREADY_EXISTS: 'category already exists!',
+  CATEGORY_NOT_FOUND: 'category not found!',
+  BRAND_ALREADY_EXISTS: 'brand already exists!',
+  BRAND_NOT_FOUND: 'brand not found!',
+  PRODUCT_NOT_FOUND: 'product not found!',
 });
